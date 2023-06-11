7dced4ac0c0346d7b2a1bcb68b779740716de960
NotTheNobleSavage <54433235+NotTheNobleSavage@users.noreply.github.com>
2023-06-11 13:24:00 +0800

    Add files via upload

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, send_file
+import spongebob
+import os
+app = Flask(__name__)
+
+#Create an endpoint that endpoint is going to be POST it is going to recive a JSON object with a prompt, send that prompt to spongebob.py and return the audio file
+@app.route("/spongebob", methods=["POST"])
+def spongebob():
+    prompt = Flask.request.json["prompt"]
+    spongebob.generate(prompt)
+    return Flask.send_file("output.wav")
+
+@app.route("/audio", methods=["GET"])
+def audio():
+    print('hi')
+    return send_file("updog.wav")
+
+app.run(threaded=True)
+
+steve = {
+    "name": "Steve",
+    "age": 25,
+}
