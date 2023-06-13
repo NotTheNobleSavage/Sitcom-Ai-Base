ad58aa16856c9a8d6aad2bfafd4958e2592fa868
NotTheNobleSavage <corey.melville88@gmail.com>
2023-06-13 21:07:50 +0800

    Server Error Checking

--- server.py
+++ server.py
@@ -2,19 +2,31 @@ from flask import Flask, send_file
 import base
 import os
 import base
+import logging
+
+#Settup up logging
+logging.basicConfig(filename='test.log', encoding='utf-8', level=logging.DEBUG)
+
 app = Flask(__name__)
 
 base.cleanup()
 @app.route("/audio", methods=["GET"])
 def audio():
+    logging.info("Audio Requested")
     base.run()
-    if os.path.exists("output.wav"):
+    try:
         return send_file("output.wav")
-    else:
-        return "Error"
+    except Exception as e:
+        logging.error(f"An unexpected error occurred while sending audio: {e}")
+        return 
 
 @app.route("/script", methods=["GET"])
 def script():
-    return send_file("script.txt")
+    logging.info("Script Requested")
+    try:
+        return send_file("script.txt")
+    except Exception as e:
+        logging.error(f"An unexpected error occurred while sending script: {e}")
+        return
 
 app.run(threaded=True)
